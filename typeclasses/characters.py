3b655a8cd6c25e4424a2db21ce94b90aa6c9641e
Xander Evans <monkeywizardx@gmail.com>
2018-06-17 16:43:05 -0500

    Fixed attribute bug.

--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -32,4 +32,4 @@ class Character(DefaultCharacter):
     """
     def at_object_creation(self):
         self.db.equipment = EquipmentHandler()
-        self.cmdsets.add(EquipmentCmdSet)
+        self.cmdset.add(EquipmentCmdSet)
